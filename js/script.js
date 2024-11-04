cc259284a93572475d7c0819f099a20497dbc1ac
Líderes Conscientes <93500932+CODABits@users.noreply.github.com>
2024-11-03 21:36:42 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -72,3 +72,14 @@ function closePopup() {
     popup.style.display = 'none';
 }
 
+// Nuevo código para cargar el JSON
+fetch('./data/lf20_aqs6jts7.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok: ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
+
